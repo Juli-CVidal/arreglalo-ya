c374a4027043ce19db6350c8c236cc33b3d53a0c
Juli-CVidal <juli.vidal.019@gmail.com>
2023-03-03 20:33:09 -0300

    Added home controller, changes in String validations

--- src/main/java/com/egg/sp/services/UsersService.java
+++ src/main/java/com/egg/sp/services/UsersService.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UsersService {
+public class UsersService{
 	
     @Autowired
     private UsersRepository usersRepository;
