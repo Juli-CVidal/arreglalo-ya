056b2a4b2a0e4e9a453238ff6e3446728ea229ad
Agustin <agsp3rs3o@gmail.com>
2023-03-13 22:04:07 -0300

    General changes

--- src/main/java/com/egg/sp/controllers/AdminController.java
+++ src/main/java/com/egg/sp/controllers/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	
 	@GetMapping("/dashboard")
-	public String adminPanel() {
+	public String adminPanel() { 
 		return "panel.html";
 	}
 
